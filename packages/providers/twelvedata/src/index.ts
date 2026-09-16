b4823ad48aa76aeb8a96bf3b6437a72d952a1c85
bjarkeef <bjarkeef@gmail.com>
2026-09-16 22:13:58 +0200

    twelvedata answers quotes, history, dividends, profiles and search

--- packages/providers/twelvedata/src/index.ts
+++ packages/providers/twelvedata/src/index.ts
@@ -1,1 +1,6 @@
-export {};
+export { TwelveDataProvider } from "./provider";
+export type { TwelveDataProviderConfig } from "./provider";
+export { TwelveDataClient } from "./client";
+export type { TwelveDataClientConfig, TwelveDataEndpoint } from "./client";
+export { appSymbolToTwelveData, twelveDataToAppSymbol } from "./exchange-map";
+export type { TwelveDataListing } from "./exchange-map";
